28fec354e6c6fd41a7be602d36c73f1d34b51341
James Grantham <james.r.grantham@gmail.com>
2019-09-30 13:24:55 +0100

    Add constructor

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [
+        {
+          item: 'test entry',
+          completed: false,
+        }
+      ],
+      newEntryForm: '',
+    }
+  }
+
   render() {
     return (
       <div>
